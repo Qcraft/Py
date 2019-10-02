87489470047312431c99d1f86ca4548b34ed80d2
Qcraft <quentinfreimanis@gmail.com>
2019-10-02 14:25:32 -0700

    fix vote.py

--- vote.py
+++ vote.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 
-age = int(input('enter your age '))
-
-if age < 3:
-	print('baby')
-elif age < 10:
-	print('child')
-elif age > 100:
-	print('dead')
-
 citizen = input('are you a citizen? ')
 
 if citizen == 'yes':
-	if age > 18:
-		print('you can vote')
-	else:
-		print('you can not vote')
+
+	age = int(input('enter your age '))
+
+	if age > 120:
+	        print('dead or vampire')
+	elif age > 17:
+        	print('you can vote')
+	elif age > 3:
+        	print('to young')
+	elif  age < 3:
+		print('baby')
 else:
-	print('ypu can not vote')
+	print('you can not vote')
+
+
+
